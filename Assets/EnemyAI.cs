0924a19240157b728fe74afd1a2ac85ec99b6656
WillBoudreau <williamboudreau804@gmail.com>
2024-02-28 12:22:10 -0400

    Created the project

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    enum EnemState
+    {
+        patrol,
+        chase,
+        search,
+        attack,
+        retreat
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemState enemyState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(enemyState) 
+        {
+            
+        }
+    }
+    public void Patrol()
+    {
+
+    }
+    public void Chase()
+    {
+
+    }
+    public void Search()
+    {
+
+    }
+    public void Attack()
+    {
+
+    }
+    public void Retreat()
+    {
+
+    }
+}
